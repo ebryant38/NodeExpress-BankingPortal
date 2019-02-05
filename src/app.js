0f1722a0b18087b7c8608b0914ea443322dfaece
Erika Bryant <erikab@a28852erikab.us.corp>
2019-02-05 17:13:23 -0500

    Module 5

--- src/app.js
+++ src/app.js
@@ -13,6 +13,12 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 
+const accountRoutes = require('./routes/accounts.js');
+const servicesRoutes = require('./routes/services.js');
+
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
+
 app.get('/', (req, res) => {
     res.render(
         'index', 
@@ -22,76 +28,11 @@ app.get('/', (req, res) => {
         });
 });
 
-app.get('/savings', (req, res) => {
-    res.render(
-        'account',
-        {
-            account : accounts.savings
-        });
-});
-
-app.get('/checking', (req, res) => {
-    res.render(
-        'account',
-        {
-            account : accounts.checking
-        });
-});
-
-app.get('/credit', (req, res) => {
-    res.render(
-        'account',
-        {
-            account : accounts.credit
-        });
-});
-
 app.get('/profile', (req, res) => {
     res.render(
         'profile',
         {
             user: users[0]
-        });
-});
-
-app.get('/transfer', (req, res) => {
-    res.render(
-        'transfer'
-    );
-});
-
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
-
-    writeJSON();
-
-    res.render(
-        'transfer',
-        {
-            message: "Transfer Completed"
-        });
-});
-
-app.get('/payment', (req, res) => {
-    res.render(
-        'payment',
-        {
-            account: accounts.credit
-        });
-});
-
-app.post('/payment', (req, res) => {
-    accounts.credit.balance = accounts.credit.balance - req.body.amount;
-    accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
-
-    writeJSON();
-
-    res.render(
-        'payment',
-        {
-            message: "Payment Successful",
-            account: accounts.credit
         });
 });
 
