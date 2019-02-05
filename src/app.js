3955b2f08a02c5fa0eb5f10b306a5c1c73b5d1a2
Erika Bryant <erikab@a28852erikab.us.corp>
2019-02-05 16:22:35 -0500

    Module 3

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
 
 // read data from file
 const accountData = fs.readFileSync(
@@ -60,6 +61,51 @@ app.get('/profile', (req, res) => {
         'profile',
         {
             user: users[0]
+        });
+});
+
+app.get('/transfer', (req, res) => {
+    res.render(
+        'transfer'
+    );
+});
+
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
+
+    const accountsJSON = JSON.stringify(accounts);
+
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+
+    res.render(
+        'transfer',
+        {
+            message: "Transfer Completed"
+        });
+});
+
+app.get('/payment', (req, res) => {
+    res.render(
+        'payment',
+        {
+            account: accounts.credit
+        });
+});
+
+app.post('/payment', (req, res) => {
+    accounts.credit.balance = accounts.credit.balance - req.body.amount;
+    accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
+
+    const accountsJSON = JSON.stringify(accounts);
+
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+
+    res.render(
+        'payment',
+        {
+            message: "Payment Successful",
+            account: accounts.credit
         });
 });
 
